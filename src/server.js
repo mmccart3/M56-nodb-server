9480b84e1228b7c7edea4cff3b5c391a101b3e87
Mark McCarthy <mm3guides@gmail.com>
2024-08-06 12:03:16 +0100

    nodb server with CREATE, READ and UPDATE

---
+++ src/server.js
@@ -0,0 +1,65 @@
+const express=require("express");
+const app=express();
+
+app.use(express.json());
+
+const listOfBooks = [];
+book_id = 1;
+
+app.post("/book", (request, response) => {
+    
+    console.log(request.body);
+    
+    const book = {
+        id: book_id,
+        title: request.body.title,
+        author: request.body.author,
+        genre: request.body.genre
+    };
+
+
+    listOfBooks.push(book);
+    book_id += 1;
+
+    const successResponse = {
+        message: "Book created successfully",
+        book: listOfBooks
+    };
+
+    response.send(successResponse)
+});
+
+app.get("/book", (request, response) => {
+    const successResponse = {
+        message: "List of books is currently: ",
+        books: listOfBooks
+    }
+
+    response.send(successResponse)
+})
+
+app.put("/book", (request, response) => {
+    function findBook(x) {
+        return x.title === request.body.title
+    }
+    const index = listOfBooks.findIndex(findBook)
+
+    if (index !== -1) {
+        listOfBooks[index].author = request.body.author;
+        listOfBooks[index].genre = request.body.genre;
+        responseMessage = {
+            msg: "author and genre updated",
+            book: listOfBooks[index]
+        }
+    } else {
+        responseMessage = {
+            msg: "Book title not found",
+            book: request.body.title
+        }
+    }
+
+    response.send(responseMessage)
+
+})
+
+app.listen(5001, () => console.log("server listening on port 5001"))
